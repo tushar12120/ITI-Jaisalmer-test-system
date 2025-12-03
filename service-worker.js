7c000a67f03f88e106f2ad4729b145a52f8d4475
tushar12120 <khatritushar777@gmail.com>
2025-12-03 21:00:47 +0530

    Add PWA support - Enable mobile installation

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'iti-jaisalmer-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/admin/login.html',
+    '/admin/dashboard.html',
+    '/student/test.html',
+    '/assets/css/style.css',
+    '/assets/js/script.js',
+    '/assets/js/admin.js',
+    '/assets/js/student.js',
+    '/assets/js/questionbank.js',
+    '/assets/images/logo.png',
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-512x512.png'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from cache first, then network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
+
+// Update service worker and clean old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
